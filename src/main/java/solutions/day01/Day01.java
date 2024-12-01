13f287569b0c0b57d58b7f0a2083af19fb22281d
liutaurasvilda <liutauras.vilda@gmail.com>
2024-12-01 19:43:09 +0000

    Rename

--- src/main/java/solutions/day01/Day01.java
+++ src/main/java/solutions/day01/Day01.java
@@ -8,26 +8,26 @@ import java.util.stream.LongStream;
 
 public class Day01 {
 
-    public static long totalDistance(HistorianNotes historianNotes) {
-        Collections.sort(historianNotes.locationIds1);
-        Collections.sort(historianNotes.locationIds2);
-        return LongStream.range(0, historianNotes.locationIds1().size()).map(e -> {
-            var locationId1 = historianNotes.locationIds1().get((int) e);
-            var locationId2 = historianNotes.locationIds2().get((int) e);
+    public static long distance(LocationIDs locationIDs) {
+        Collections.sort(locationIDs.leftList);
+        Collections.sort(locationIDs.rightList);
+        return LongStream.range(0, locationIDs.leftList.size()).map(e -> {
+            var locationId1 = locationIDs.leftList.get((int) e);
+            var locationId2 = locationIDs.rightList.get((int) e);
             return Math.abs(locationId1 - locationId2);
         }).sum();
     }
 
-    public static long similarityScore(HistorianNotes historianNotes) {
-        var locationIDs2Counts = historianNotes.locationIds2.stream()
+    public static long similarityScore(LocationIDs locationIDs) {
+        var locationIDs2Counts = locationIDs.rightList.stream()
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
-        return historianNotes.locationIds1.stream()
+        return locationIDs.leftList.stream()
                 .map(e -> locationIDs2Counts.getOrDefault(e, 0L) * e)
                 .mapToLong(e -> e)
                 .sum();
     }
 
-    public record HistorianNotes(List<Long> locationIds1, List<Long> locationIds2) {
+    public record LocationIDs(List<Long> leftList, List<Long> rightList) {
     }
 }
--- src/test/java/solutions/day01/Day01Test.java
+++ src/test/java/solutions/day01/Day01Test.java
@@ -10,8 +10,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class Day01Test {
 
     @Test
-    void totalDistance() {
-        assertEquals(1580061, Day01.totalDistance(historianNotes()));
+    void distance() {
+        assertEquals(1580061, Day01.distance(historianNotes()));
     }
 
     @Test
@@ -19,16 +19,16 @@ class Day01Test {
         assertEquals(23046913, Day01.similarityScore(historianNotes()));
     }
 
-    private static Day01.HistorianNotes historianNotes() {
-        var locationIds1 = new ArrayList<Long>();
-        var locationIds2 = new ArrayList<Long>();
+    private static Day01.LocationIDs historianNotes() {
+        var leftList = new ArrayList<Long>();
+        var rightList = new ArrayList<Long>();
 
         ResourceReader.asString("day01.txt").stream()
                 .map(e -> e.split("   ")).forEach(e -> {
-                    locationIds1.add(Long.valueOf(e[0]));
-                    locationIds2.add(Long.valueOf(e[1]));
+                    leftList.add(Long.valueOf(e[0]));
+                    rightList.add(Long.valueOf(e[1]));
                 });
 
-        return new Day01.HistorianNotes(locationIds1, locationIds2);
+        return new Day01.LocationIDs(leftList, rightList);
     }
 }
