2d5cfa221f4605bf2cccc2ce72327e2e71be8971
liutaurasvilda <liutauras.vilda@gmail.com>
2024-12-01 20:05:25 +0000

    Renaming

--- src/test/java/solutions/day01/Day01Test.java
+++ src/test/java/solutions/day01/Day01Test.java
@@ -11,15 +11,15 @@ class Day01Test {
 
     @Test
     void distance() {
-        assertEquals(1580061, Day01.distance(historianNotes()));
+        assertEquals(1580061, Day01.distance(locationIDs()));
     }
 
     @Test
     void similarityScore() {
-        assertEquals(23046913, Day01.similarityScore(historianNotes()));
+        assertEquals(23046913, Day01.similarityScore(locationIDs()));
     }
 
-    private static Day01.LocationIDs historianNotes() {
+    private static Day01.LocationIDs locationIDs() {
         var leftList = new ArrayList<Long>();
         var rightList = new ArrayList<Long>();
 
