44eed0ddcdf7e3951fae9f38492eb1245afbfe2b
liutaurasvilda <liutauras.vilda@gmail.com>
2024-12-08 22:29:55 +0000

    day07 init

--- src/main/java/solutions/day07/Day07.java
+++ src/main/java/solutions/day07/Day07.java
@@ -1,11 +1,41 @@
 package solutions.day07;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 final class Day07 {
 
     public static long calibrationResult(List<Equation> equations) {
-        return 0;
+        return equations.stream()
+                .map(Day07::evaluate)
+                .filter(Optional::isPresent)
+                .mapToLong(Optional::get)
+                .sum();
+    }
+
+    private static Optional<Long> evaluate(Equation equation) {
+        var results = new ArrayList<Long>();
+        var operatorsPermutations = permutations(List.of("+", "*"), equation.numbers().size() - 1);
+        for (List<String> operators : operatorsPermutations) {
+            var result = equation.numbers().getFirst();
+            for (int j = 1; j < equation.numbers().size(); j++) {
+                var operator = operators.get(j - 1);
+                if (operator.equals("+")) {
+                    result += equation.numbers().get(j);
+                } else if (operator.equals("*")) {
+                    result *= equation.numbers().get(j);
+                }
+            }
+            results.add(result);
+        }
+        return results.stream().filter(e -> e == equation.testValue()).findFirst();
+    }
+
+    public static List<List<String>> permutations(List<String> operators, int length) {
+        var result = new ArrayList<List<String>>();
+        // TODO build permutations
+        return result;
     }
 
     record Equation(long testValue, List<Long> numbers) {
--- src/test/java/solutions/day07/Day07Test.java
+++ src/test/java/solutions/day07/Day07Test.java
@@ -3,6 +3,7 @@ package solutions.day07;
 import org.junit.jupiter.api.Test;
 import util.ResourceReader;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,6 +14,22 @@ class Day07Test {
     @Test
     void part1() {
         assertEquals(3749, Day07.calibrationResult(equations()));
+    }
+
+    @Test
+    void permutations() {
+        var expected1 = new ArrayList<List<String>>();
+        expected1.add(List.of("+"));
+        expected1.add(List.of("*"));
+
+        var expected2 = new ArrayList<List<String>>();
+        expected2.add(List.of("+", "+"));
+        expected2.add(List.of("+", "*"));
+        expected2.add(List.of("*", "+"));
+        expected2.add(List.of("*", "*"));
+
+        assertEquals(expected1, Day07.permutations(List.of("+", "*"), 1));
+        assertEquals(expected2, Day07.permutations(List.of("+", "*"), 2));
     }
 
     private List<Day07.Equation> equations() {
