fc72a755307c29b76e426d820c0056c64a496029
liutaurasvilda <liutauras.vilda@gmail.com>
2024-12-12 23:34:09 +0000

    Day11 prep

---
+++ src/main/java/solutions/day11/Day11.java
@@ -0,0 +1,12 @@
+package solutions.day11;
+
+import java.util.List;
+
+final class Day11 {
+
+    private static final int BLINKS = 25;
+
+    public static long stonesCount(List<Integer> stones) {
+        return 0;
+    }
+}
---
+++ src/test/java/solutions/day11/Day11Test.java
@@ -0,0 +1,20 @@
+package solutions.day11;
+
+import org.junit.jupiter.api.Test;
+import util.ResourceReader;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day11Test {
+
+    @Test
+    void part1() {
+        assertEquals(0L, Day11.stonesCount(stones()));
+    }
+
+    private List<Integer> stones() {
+        return ResourceReader.oneLineIntsDelimited("day11_test.txt", ResourceReader.Delimiter.SPACE);
+    }
+}
