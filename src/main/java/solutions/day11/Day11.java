33319c728fcb971ac26001c81886dd3fe2090e0b
liutaurasvilda <liutauras.vilda@gmail.com>
2024-12-14 01:42:55 +0000

    refactor

--- src/main/java/solutions/day11/Day11.java
+++ src/main/java/solutions/day11/Day11.java
@@ -2,33 +2,38 @@ package solutions.day11;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 final class Day11 {
 
     public static long stonesCount(int blinks, List<String> stones) {
-        var currentMapping = stones.stream().collect(Collectors.toMap(e -> e, e -> 1L));
+        var mapping = stones.stream().collect(Collectors.toMap(e -> e, e -> 1L));
         for (int i = 0; i < blinks; i++) {
-            var newMapping = new HashMap<String, Long>();
-            for (var stoneEntry : currentMapping.entrySet()) {
-                var stone = stoneEntry.getKey();
-                var stoneCount = stoneEntry.getValue();
-                if (stone.equals("0")) {
-                    newMapping.merge("1", stoneCount, Long::sum);
-                } else if ((stone.length() & 1) == 0) {
-                    var left = stone.substring(0, stone.length() / 2);
-                    var right = stone.substring(stone.length() / 2);
-                    while (right.startsWith("0") && !right.equals("0")) {
-                        right = right.substring(1);
-                    }
-                    newMapping.merge(left, stoneCount, Long::sum);
-                    newMapping.merge(right, stoneCount, Long::sum);
-                } else {
-                    newMapping.merge(String.valueOf(Long.parseLong(stone) * 2024), stoneCount, Long::sum);
-                }
-            }
-            currentMapping = newMapping;
+            mapping = blink(mapping);
         }
-        return currentMapping.values().stream().mapToLong(e -> e).sum();
+        return mapping.values().stream().mapToLong(e -> e).sum();
+    }
+
+    private static HashMap<String, Long> blink(Map<String, Long> mapping) {
+        var newMapping = new HashMap<String, Long>();
+        for (var entry : mapping.entrySet()) {
+            var stone = entry.getKey();
+            var stoneCount = entry.getValue();
+            if (stone.equals("0")) {
+                newMapping.merge("1", stoneCount, Long::sum);
+            } else if ((stone.length() & 1) == 0) {
+                var left = stone.substring(0, stone.length() / 2);
+                var right = stone.substring(stone.length() / 2);
+                while (right.startsWith("0") && !right.equals("0")) {
+                    right = right.substring(1);
+                }
+                newMapping.merge(left, stoneCount, Long::sum);
+                newMapping.merge(right, stoneCount, Long::sum);
+            } else {
+                newMapping.merge(String.valueOf(Long.parseLong(stone) * 2024), stoneCount, Long::sum);
+            }
+        }
+        return newMapping;
     }
 }
