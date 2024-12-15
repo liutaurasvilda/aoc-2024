03023d45acdf5635c9da6934acea8a9b2db8ed66
liutaurasvilda <liutauras.vilda@gmail.com>
2024-12-15 16:56:07 +0000

    Day13 part1

--- src/main/java/solutions/day13/Day13.java
+++ src/main/java/solutions/day13/Day13.java
@@ -12,8 +12,25 @@ final class Day13 {
     private static final int B_BUTTON_TOKENS = 1;
 
     public static long fewestTokens(List<ClawMachine> clawMachines) {
-        // TODO
-        return 0;
+        var result = 0;
+        for (ClawMachine clawMachine : clawMachines) {
+            for (int aPress = 0; aPress < MAX_BUTTON_PRESS; aPress++) {
+                for (int bPress = 0; bPress < MAX_BUTTON_PRESS; bPress++) {
+                    var aX = clawMachine.a().x().apply(aPress);
+                    var bX = clawMachine.b().x().apply(bPress);
+                    var abX = aX + bX;
+
+                    var aY = clawMachine.a().y().apply(aPress);
+                    var bY = clawMachine.b().y().apply(bPress);
+                    var abY = aY + bY;
+
+                    if (clawMachine.prize().equals(new Coordinate(abX, abY))) {
+                        result += aPress * A_BUTTON_TOKENS + bPress * B_BUTTON_TOKENS;
+                    }
+                }
+            }
+        }
+        return result;
     }
 
     record ClawMachine(Button a, Button b, Coordinate prize) {
--- src/test/java/solutions/day13/Day13Test.java
+++ src/test/java/solutions/day13/Day13Test.java
@@ -13,14 +13,14 @@ class Day13Test {
 
     @Test
     void part1() {
-        assertEquals(480, Day13.fewestTokens(clawMachines()));
+        assertEquals(32067, Day13.fewestTokens(clawMachines()));
     }
 
     @Test
     void part1_machine1() {
         var clawMachine = new Day13.ClawMachine(
-                new Day13.ClawMachine.Button("A", x -> x + 94, y -> y + 34),
-                new Day13.ClawMachine.Button("B", x -> x + 22, y -> y + 67),
+                new Day13.ClawMachine.Button("A", x -> x * 94, y -> y * 34),
+                new Day13.ClawMachine.Button("B", x -> x * 22, y -> y * 67),
                 new Coordinate(8400, 5400)
         );
         assertEquals(280, Day13.fewestTokens(List.of(clawMachine)));
@@ -29,8 +29,8 @@ class Day13Test {
     @Test
     void part1_machine2() {
         var clawMachine = new Day13.ClawMachine(
-                new Day13.ClawMachine.Button("A", x -> x + 26, y -> y + 66),
-                new Day13.ClawMachine.Button("B", x -> x + 67, y -> y + 21),
+                new Day13.ClawMachine.Button("A", x -> x * 26, y -> y * 66),
+                new Day13.ClawMachine.Button("B", x -> x * 67, y -> y * 21),
                 new Coordinate(12748, 12176)
         );
         assertEquals(0, Day13.fewestTokens(List.of(clawMachine)));
@@ -39,8 +39,8 @@ class Day13Test {
     @Test
     void part1_machine3() {
         var clawMachine = new Day13.ClawMachine(
-                new Day13.ClawMachine.Button("A", x -> x + 17, y -> y + 86),
-                new Day13.ClawMachine.Button("B", x -> x + 84, y -> y + 37),
+                new Day13.ClawMachine.Button("A", x -> x * 17, y -> y * 86),
+                new Day13.ClawMachine.Button("B", x -> x * 84, y -> y * 37),
                 new Coordinate(7870, 6450)
         );
         assertEquals(200, Day13.fewestTokens(List.of(clawMachine)));
@@ -49,15 +49,15 @@ class Day13Test {
     @Test
     void part1_machine4() {
         var clawMachine = new Day13.ClawMachine(
-                new Day13.ClawMachine.Button("A", x -> x + 69, y -> y + 23),
-                new Day13.ClawMachine.Button("B", x -> x + 27, y -> y + 71),
+                new Day13.ClawMachine.Button("A", x -> x * 69, y -> y * 23),
+                new Day13.ClawMachine.Button("B", x -> x * 27, y -> y * 71),
                 new Coordinate(18641, 10279)
         );
         assertEquals(0, Day13.fewestTokens(List.of(clawMachine)));
     }
 
     private List<Day13.ClawMachine> clawMachines() {
-        var input = ResourceReader.stringLines("day13_test.txt");
+        var input = ResourceReader.stringLines("day13.txt");
         var clawMachines = new ArrayList<Day13.ClawMachine>();
         for (int i = 0; i < input.size(); i = i + 3) {
             var a = input.get(i);
@@ -83,8 +83,8 @@ class Day13Test {
             var prizeY = Integer.parseInt(prizeRightSplit[1]);
 
             var clawMachine = new Day13.ClawMachine(
-                    new Day13.ClawMachine.Button(aLabel, x -> x + aX, y -> y + aY),
-                    new Day13.ClawMachine.Button(bLabel, x -> x + bX, y -> y + bY),
+                    new Day13.ClawMachine.Button(aLabel, x -> x * aX, y -> y * aY),
+                    new Day13.ClawMachine.Button(bLabel, x -> x * bX, y -> y * bY),
                     new Coordinate(prizeX, prizeY)
             );
             clawMachines.add(clawMachine);
