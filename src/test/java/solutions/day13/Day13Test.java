2b88e2a0baea3f60b62e33e7a30a548977b67022
liutaurasvilda <liutauras.vilda@gmail.com>
2024-12-15 18:16:42 +0000

    Day13 prep for part 2

--- src/test/java/solutions/day13/Day13Test.java
+++ src/test/java/solutions/day13/Day13Test.java
@@ -14,13 +14,13 @@ class Day13Test {
 
     @Test
     void part1() {
-        assertEquals(32067, Day13.fewestTokens(clawMachines1()));
+        assertEquals(32067, Day13.fewestTokens(clawMachines()));
     }
 
     @Test
     @Disabled
     void part2() {
-        assertEquals(0, Day13.fewestTokens(clawMachines2()));
+        assertEquals(0, Day13.fewestTokens(clawMachines()));
     }
 
     @Test
@@ -63,7 +63,7 @@ class Day13Test {
         assertEquals(0, Day13.fewestTokens(List.of(clawMachine)));
     }
 
-    private List<Day13.ClawMachine> clawMachines1() {
+    private List<Day13.ClawMachine> clawMachines() {
         var input = ResourceReader.stringLines("day13.txt");
         var clawMachines = new ArrayList<Day13.ClawMachine>();
         for (int i = 0; i < input.size(); i = i + 3) {
@@ -88,44 +88,6 @@ class Day13Test {
             var prizeRightSplit = prizeSplit[1].split("=");
             var prizeX = Long.parseLong(prizeLeftSplit[1]);
             var prizeY = Long.parseLong(prizeRightSplit[1]);
-
-            var clawMachine = new Day13.ClawMachine(
-                    new Day13.ClawMachine.Button(aLabel, x -> x * aX, y -> y * aY),
-                    new Day13.ClawMachine.Button(bLabel, x -> x * bX, y -> y * bY),
-                    new Coordinate(prizeX, prizeY)
-            );
-            clawMachines.add(clawMachine);
-
-            i++;
-        }
-        return clawMachines;
-    }
-
-    private List<Day13.ClawMachine> clawMachines2() {
-        var input = ResourceReader.stringLines("day13.txt");
-        var clawMachines = new ArrayList<Day13.ClawMachine>();
-        for (int i = 0; i < input.size(); i = i + 3) {
-            var a = input.get(i);
-            var b = input.get(i+1);
-            var prize = input.get(i+2);
-
-            var aSplit = a.split(": ");
-            var aLabel = aSplit[0].substring(aSplit[0].length()-1);
-            var aF = aSplit[1].split(", ");
-            var aX = Long.parseLong(aF[0].split("\\+")[1]);
-            var aY = Long.parseLong(aF[1].split("\\+")[1]);
-
-            var bSplit = b.split(": ");
-            var bLabel = bSplit[0].substring(bSplit[0].length()-1);
-            var bF = bSplit[1].split(", ");
-            var bX = Long.parseLong(bF[0].split("\\+")[1]);
-            var bY = Long.parseLong(bF[1].split("\\+")[1]);
-
-            var prizeSplit = prize.split(", ");
-            var prizeLeftSplit = prizeSplit[0].split("=");
-            var prizeRightSplit = prizeSplit[1].split("=");
-            var prizeX = 10000000000000L + Long.parseLong(prizeLeftSplit[1]);
-            var prizeY = 10000000000000L + Long.parseLong(prizeRightSplit[1]);
 
             var clawMachine = new Day13.ClawMachine(
                     new Day13.ClawMachine.Button(aLabel, x -> x * aX, y -> y * aY),
