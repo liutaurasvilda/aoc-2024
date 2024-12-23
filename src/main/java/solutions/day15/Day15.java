c089425bac57954971b34de4a5f39fda415b77a9
liutaurasvilda <liutauras.vilda@gmail.com>
2024-12-23 15:53:28 +0000

    Day15 prep

---
+++ src/main/java/solutions/day15/Day15.java
@@ -0,0 +1,8 @@
+package solutions.day15;
+
+final class Day15 {
+
+    public static long sumOfGpsCoordinates() {
+        return 0;
+    }
+}
---
+++ src/test/java/solutions/day15/Day15Test.java
@@ -0,0 +1,13 @@
+package solutions.day15;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day15Test {
+
+    @Test
+    void part1() {
+        assertEquals(0, Day15.sumOfGpsCoordinates());
+    }
+}
